e1f826b76ef18954805bae072d4509ebb7acc89c
pohern <ohern_p@yahoo.com>
2021-10-12 15:07:47 -0500

    Try first two examples

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import 'handsontable/dist/handsontable.full.css';
 import './App.css';
+import { HotTable } from "@handsontable/react";
+import Handsontable from 'handsontable';
 
+// const hotData = [
+//   ["", "Tesla", "Volvo", "Toyota", "Honda"],
+//   ["2020", 10, 11, 12, "=(B2+B3)"],
+//   ["2021", 20, 11, 14, 13],
+//   ["2022", 30, 15, 12, 13],
+// ];
+const hotData = Handsontable.helper.createSpreadsheetData(6, 10);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div id="hot-app">
+    //   <HotTable
+    //     data={hotData}
+    //     colHeaders={true}
+    //     rowHeaders={true}
+    //     width="600"
+    //     height="300"
+    //   />
+    // </div>
+    <div>
+      <HotTable
+        data={hotData}
+        colHeaders={true}
+        rowHeaders={true}
+        height="auto"
+        licenseKey="non-commercial-and-evaluation"
+      />
     </div>
   );
 }
